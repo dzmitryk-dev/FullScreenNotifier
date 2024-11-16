4ea2a113bddda98226682e29e7f02625739afded
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2024-11-16 10:19:02 +0100

    Initial commit

---
+++ app/src/main/kotlin/dzmitryk/app/template/MainActivity.kt
@@ -0,0 +1,66 @@
+package dzmitryk.app.template
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import dzmitryk.app.template.ui.hello.Greeting
+import dzmitryk.app.template.ui.hello.RainbowAnnotated
+import dzmitryk.app.template.ui.hello.RainbowColorChangeText
+import dzmitryk.app.template.ui.hello.RainbowColorText
+import dzmitryk.app.template.ui.theme.AppTemplateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val text = "Hello World!!"
+        setContent {
+            AppTemplateTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        item {
+                            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                                Greeting(text = text)
+                            }
+                        }
+                        item {
+                            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                                RainbowColorChangeText(
+                                    text = text
+                                )
+                            }
+                        }
+                        item {
+                            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                                RainbowAnnotated(
+                                    text = text
+                                )
+                            }
+                        }
+                        item {
+                            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                                RainbowColorText(text = text)
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
